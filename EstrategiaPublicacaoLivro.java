ea2d04364351efbea2ab85cb44173805aa5f17fe
joaovictordejesus <166874133+joaovictordejesus@users.noreply.github.com>
2025-01-07 16:41:47 -0300

    Create EstrategiaPublicacaoLivro.java

---
+++ EstrategiaPublicacaoLivro.java
@@ -0,0 +1,7 @@
+// João Victor de Jesus Augusto
+public class EstrategiaPublicacaoLivro implements PublicavelInterface {
+    @Override
+    public void publicar() {
+        System.out.println("Publicando um livro...");
+    }
+}
