339f4143964b430a1f6780fbbad320508fad71e1
joaovictordejesus <166874133+joaovictordejesus@users.noreply.github.com>
2025-01-07 16:42:23 -0300

    Create EstrategiaPublicacaoArtigo.java

---
+++ EstrategiaPublicacaoArtigo.java
@@ -0,0 +1,7 @@
+// João Victor de Jesus Augusto
+public class EstrategiaPublicacaoArtigo implements PublicavelInterface {
+    @Override
+    public void publicar() {
+        System.out.println("Publicando um artigo...");
+    }
+}
